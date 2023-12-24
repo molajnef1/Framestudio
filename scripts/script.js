86515f21a8c34545ae3e370a85460899406cb66b
molajnef1 <mohamed.neji.lajnef@gmail.com>
2023-12-24 23:28:30 +0100

    making changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('nav a').forEach(link => {
-  console.log('Active Page:', activePage);
-  console.log('Link Href:', link.href);
+
 
   if (link.href.includes(activePage)) {
     link.classList.add('active');
