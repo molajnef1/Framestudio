6d341e01ac413499e7eaeae06e451581fa1b8d22
Mo <mo@MacBook-Air-de-Sara.local>
2023-12-27 05:37:04 +0100

    commiting initial mobile changes that is looking good

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menuIcon = document.getElementById('menu-icon');
-  const mainMenu = document.querySelector('.nav-container ul');
-
-  if (menuIcon && mainMenu) {
-      menuIcon.addEventListener('click', function () {
-          mainMenu.classList.toggle('show-menu');
-      });
-  }
-});
+function toggleMobileMenu(menu)
+{
+  menu.classList.toggle('open');
+}
